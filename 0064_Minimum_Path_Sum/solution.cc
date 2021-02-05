2e0d47f5004b9ee4eaf8b29865212bc08f76e000
JiaJie LIN <august.jjlin@gmail.com>
2021-02-06 00:18:14 +0800

    prob 64

---
+++ 0064_Minimum_Path_Sum/solution.cc
@@ -0,0 +1,33 @@
+#include <vector>
+#include <climits>
+#include <algorithm>
+
+using std::vector;
+
+class Solution {
+ public:
+    int minPathSum(const vector<vector<int> >& grid) {
+        if (grid.empty() || grid[0].empty()) return 0;
+
+        int m = grid.size(), n = grid[0].size();
+
+        vector<vector<int> > dp(m, vector<int>(n, INT_MAX));
+        dp[0][0] = grid[0][0];
+
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (i == 0 && j == 0) {
+                    continue;
+                } else if (i == 0) {
+                    dp[i][j] = grid[i][j] + dp[i][j - 1];
+                } else if (j == 0) {
+                    dp[i][j] = grid[i][j] + dp[i - 1][j];
+                } else {
+                    dp[i][j] = grid[i][j] + std::min(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[m - 1][n - 1];
+    }
+};
