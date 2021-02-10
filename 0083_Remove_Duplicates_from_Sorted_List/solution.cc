2e3a9424bc08a27fbcc6f5e0012e3ef51faf4b2f
JiaJie LIN <august.jjlin@gmail.com>
2021-02-10 20:59:50 +0800

    prob 83

---
+++ 0083_Remove_Duplicates_from_Sorted_List/solution.cc
@@ -0,0 +1,14 @@
+class Solution {
+ public:
+    struct ListNode {
+        int val;
+        ListNode *next;
+        ListNode() : val(0), next(nullptr) {}
+        ListNode(int x) : val(x), next(nullptr) {}
+        ListNode(int x, ListNode *next) : val(x), next(next) {}
+    };
+
+    ListNode* deleteDuplicates(ListNode* head) {
+        
+    }
+};
